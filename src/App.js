8d94d2d2fea7e47293c09c5ffda45b6fba50773d
Vivian Bow <vivianbao1009@gmail.com>
2022-02-02 23:54:01 +0900

    Added component folder, with main component files, and App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ProfilePic from "./components/ProfilePic"
+import Main from "./components/Main"
+import Footer from "./components/Footer"
+
+export default function App() {
+  return(
+    <>
+      <ProfilePic />
+      <Main />
+      <Footer />
+    </>
+  )
+}
---
+++ src/components/ProfilePic.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function ProfilePic() {
+  return(
+    <img src="./images/logo512.png" alt="profile pic"/>
+  )
+}
